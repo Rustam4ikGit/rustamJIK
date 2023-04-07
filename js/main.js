cd95c64098ff059965afdcd06696b441ab1d03a2
Рустам <rustam@MacBook-Air-Rustam.local>
2023-04-07 21:04:11 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// let student = {
+//     name: "Rustam",
+//     gender: "male",
+//     age: 13,
+//     isMarried: true,
+//     haveCar: true,
+//     passport: {
+//         serres: "AC",
+//         number: +998913197989,
+//         data: '2040-20-20',
+//         exp: '2050-20-20'
+//     },
+//     goSchool: true,
+//     wives: ['Nargiza','Angelica','Sabrina'],
+//     country: "Uzbekistan",
+//     city: "Samarkand",
+//     haveCar: ['malibu','centra','lamborgini','bugati','mazerati','tesla'],
+//     haveCompany: ['Apple','Microsoft_Office','YouTube','Samsung']
+// }
+
+
+
+
+
